e640bc173a856cb680c79625084b253e50ef1a78
joneyin <yin_jone@163.com>
2017-05-04 16:25:37 +0800

    第一次提交

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Jone Yin on 2017/5/4.
+//  Copyright © 2017年 Jone Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Jone Yin on 2017/5/4.
+//  Copyright © 2017年 Jone Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
